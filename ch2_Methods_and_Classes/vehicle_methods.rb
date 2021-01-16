d1eec6755c27b0b58670493cf70dc5a04b5342ab
MIJanda <mokiliisaacjanda@gmail.com>
2021-01-17 01:06:02 +0400

    implemented optional parameters

--- ch2_Methods_and_Classes/vehicle_methods.rb
+++ ch2_Methods_and_Classes/vehicle_methods.rb
@@ -8,11 +8,7 @@ def sound_horn
     puts "Beep! Beep!"
 end 
 
-def use_headlights(brightness)
+def use_headlights(brightness = "low-beam")
     puts "Turning on #{brightness} headlights"
     puts "Watch out for deer!"
 end
-
-accelerate
-sound_horn
-use_headlights("high-beam")
